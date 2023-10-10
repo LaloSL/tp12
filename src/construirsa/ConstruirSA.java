d847cf10773548d8ca0d9c116ed4ca7e804d907a
JoaquinOrtega505 <72822153+JoaquinOrtega505@users.noreply.github.com>
2023-10-10 17:28:01 -0300

    Ejercicio 3

--- src/construirsa/ConstruirSA.java
+++ src/construirsa/ConstruirSA.java
@@ -37,8 +37,24 @@ public class ConstruirSA {
                 //string2: Nombre de usuario de conexion a la BD (por defecto es root)
                 //string3: contraseña (en este caso comillas vacias xq no hay contraseña)
                 
+                //Ejercicio 3:
                 
-          
+//            String sql1 = "INSERT INTO empleado(id_empleado, dni, apellido, nombre, acceso, estado)"
+//                 + "VALUES (11, 123456, 'Torre', 'Victor', 1, 1)";
+//                 String sql2 = "INSERT INTO empleado(id_empleado, dni, apellido, nombre, acceso, estado)"
+//                 + "VALUES (12, 654321, 'Gomez', 'Juana', 2, 0)";
+//                 String  sql3 = "INSERT INTO empleado(id_empleado, dni, apellido, nombre, acceso, estado)"
+//                 + "VALUES (13, 7890, 'Sosa', 'Pedro', 1, 1)";
+//                
+//                 PreparedStatement ps1 = conexion.prepareStatement(sql1);
+//                 ps1.executeUpdate();
+//                
+//                 PreparedStatement ps2 = conexion.prepareStatement(sql2);
+//                 ps2.executeUpdate();
+//            
+//                 PreparedStatement ps3 = conexion.prepareStatement(sql3);
+//                 ps3.executeUpdate();    
+//          
             
             } catch (SQLException ex) {
                 JOptionPane.showMessageDialog(null,"Error al cargar Driver"+ex.getMessage());
